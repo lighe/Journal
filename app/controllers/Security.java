7fa3e2a6de7de3b90203998bcd66f0c670904d86
Alexandru Lighezan <alighe89@gmail.com>
2012-03-29 19:47:33 +0100

    first commit

---
+++ app/controllers/Security.java
@@ -0,0 +1,11 @@
+package controllers;
+import models.*;
+ 
+public class Security extends Secure.Security {
+    
+    static boolean authenticate(String username, String password) {
+        User user = User.find("byEmail", username).first();
+        return user != null && user.password.equals(password);
+    }    
+    
+}
