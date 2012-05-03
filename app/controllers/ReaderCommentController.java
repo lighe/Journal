5356a9a8e0b03c202e50e42d7e023ec6691b5de7
Ian Morrissey <aca08im@sheffield.ac.uk>
2012-05-04 00:05:46 +0100

    started on adding the form for user comments, however can't get it to work due to an error regarding saving the details

--- app/controllers/ReaderCommentController.java
+++ app/controllers/ReaderCommentController.java
@@ -8,10 +8,10 @@ public class ReaderCommentController extends Controller {
     public static void readerComments (String emailAddress, String message){
       
                    
-        ReaderComment addcomment = new ReaderComment(emailAddress, message);
-        addcomment.save();
+        ReaderComment readerMessage = new ReaderComment(emailAddress, message);
+        readerMessage.save();
         flash.success("Thank you, your comment has been sent to an editor");
-        render ("ReaderCommentController/index.html", addcomment);
+        render ("ReaderCommentController/index.html", readerMessage);
         
         
     }
